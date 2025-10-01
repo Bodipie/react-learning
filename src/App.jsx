0946fef9d63db129fbe453473188658222969ee5
Abdalla <bodi.tarek.bt@gmail.com>
2025-10-01 21:47:41 +0300

    Modified App.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -1,10 +1,18 @@
 import './App.css';
+import TheHeader from './components/TheHeader/TheHeader';
+import TheFooter from './components/TheFooter/TheFooter';
 
 function App() {
 
   return (
-    <div>
-      <h1>Hello</h1>
+    <div className='app'>
+      <TheHeader />
+      <main>
+        <section>
+          Lorem ipsum dolor sit amet consectetur adipisicing elit. Optio vitae ducimus blanditiis magnam commodi quod sit. Alias, quos inventore. Eaque rem in expedita atque eveniet harum repellendus aspernatur, omnis esse.
+        </section>
+      </main>
+      <TheFooter />
     </div>
   );
 }
